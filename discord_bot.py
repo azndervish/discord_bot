9b18997701e1c2e70034c73462137c72357c9118
Daniel Luh <danielluh@gmail.com>
2023-06-18 13:29:13 -0400

    echo

--- discord_bot.py
+++ discord_bot.py
@@ -26,15 +26,18 @@ async def ping(ctx):
 @bot.command()
 async def ask(ctx):
     question = ctx.message.content
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": question}
-        ]
-    )
-    answer = response['choices'][0]['message']['content']
-    await ctx.send(answer)
+    print(question)
+    await ctx.send(question)
+
+#   response = openai.ChatCompletion.create(
+#       model='gpt-3.5-turbo',
+#       messages=[
+#           {"role": "system", "content": "You are a helpful assistant."},
+#           {"role": "user", "content": question}
+#       ]
+#   )
+#   answer = response['choices'][0]['message']['content']
+#   await ctx.send(answer)
 
 # Run the bot with your token
 bot.run(os.environ['DISCORD_API_KEY'])
