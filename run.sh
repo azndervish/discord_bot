6bb13be829b78fa15a7cfdc37f4ff6a4822e1244
Daniel Luh <danielluh@gmail.com>
2023-06-18 13:40:05 -0400

    fix key location

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export DISCORD_API_KEY=`aws ssm get-parameter --name discord_bot --region us-east-2 --with-decryption | jq -r .Parameter.Value`
-export OPENAI_API_KEY=`aws ssm get-parameter --name discord_bot --region us-east-2 --with-decryption | jq -r .Parameter.Value`
+export OPENAI_API_KEY=`aws ssm get-parameter --name openai_api_key --region us-east-2 --with-decryption | jq -r .Parameter.Value`
 python3 discord_bot.py
