d72e0233d48e8fa22b15b2e74bd571988383304e
Daniel Luh <danielluh@gmail.com>
2023-06-18 22:42:17 -0400

    add guessin game init

--- discord_bot.py
+++ discord_bot.py
@@ -48,14 +48,12 @@ async def ask(ctx):
 async def guess_init(ctx):
     guess_messages = [{"role": "user", "content": GUESS_INIT_PROMPT}]
     response = openai.ChatCompletion.create( model='gpt-3.5-turbo', messages=guess_messages )
-    guess_messages.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-    guess_messages.append({"role": "user", "content": "Tell me what the one-word solution is."})
-    response = openai.ChatCompletion.create( model='gpt-3.5-turbo', messages=guess_messages )
-    guess_messages.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-    guess_messages.append({"role": "user", "content": "Give me a hint to start."})
-    response = openai.ChatCompletion.create( model='gpt-3.5-turbo', messages=guess_messages )
-    await return_openai_response(ctx, response)
-    await ctx.send(guess_messages)
+    initial_response = response['choices'][0]['message']['content']
+    output = []
+    for line in initial_response.split('\n'):
+        if "solution" not in line.lower():
+            output.append(line)
+    await ctx.send("\n".join(output))
 
 # Command: Make a guess in the Guessing Game
 @bot.command()
@@ -66,10 +64,12 @@ async def guess(ctx):
         messages=guess_messages 
     )
     await return_openai_response(ctx, response)
-    await ctx.send(guess_messages)
 
 async def return_openai_response(ctx, response):
     answer = response['choices'][0]['message']['content']
+    await return_response(ctx, answer)
+
+async def return_response(ctx, answer):
     last_sent_index = 0
     while last_sent_index < len(answer):
         end_index = last_sent_index + DISCORD_MAX_LENGTH
